5cf8dabebe0a49a30b716f905a9d3a08d94517cc
Thilanka Thushan <158076487+thilankathushan@users.noreply.github.com>
2026-04-28 12:32:54 +0530

    fix duplicate settings route in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,15 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider, useAuth } from './context/AuthContext';
-import { ThemeProvider } from './context/ThemeContext';
-import Navbar   from './components/Navbar';
-import Login    from './pages/Login';
-import Register from './pages/Register';
-import Feed     from './pages/Feed';
-import Explore  from './pages/Explore';
-import Profile  from './pages/Profile';
+import { ThemeProvider }         from './context/ThemeContext';
+import Navbar          from './components/Navbar';
+import Login           from './pages/Login';
+import Register        from './pages/Register';
+import Feed            from './pages/Feed';
+import Explore         from './pages/Explore';
+import Profile         from './pages/Profile';
 import AccountSettings from './pages/AccountSettings';
 import ForgotPassword  from './pages/ForgotPassword';
 import ResetPassword   from './pages/ResetPassword';
-
 
 function PrivateRoute({ children }) {
   const { user } = useAuth();
@@ -28,16 +27,15 @@ function AppRoutes() {
     <>
       {user && <Navbar />}
       <Routes>
-        <Route path="/login"    element={<PublicRoute><Login /></PublicRoute>} />
-        <Route path="/register" element={<PublicRoute><Register /></PublicRoute>} />
-        <Route path="/"         element={<PrivateRoute><Feed /></PrivateRoute>} />
-        <Route path="/explore"  element={<PrivateRoute><Explore /></PrivateRoute>} />
+        <Route path="/login"           element={<PublicRoute><Login /></PublicRoute>} />
+        <Route path="/register"        element={<PublicRoute><Register /></PublicRoute>} />
+        <Route path="/"                element={<PrivateRoute><Feed /></PrivateRoute>} />
+        <Route path="/explore"         element={<PrivateRoute><Explore /></PrivateRoute>} />
         <Route path="/profile/:username" element={<PrivateRoute><Profile /></PrivateRoute>} />
-        <Route path="*" element={<Navigate to="/" />} />
-        <Route path="/settings" element={<PrivateRoute><AccountSettings /></PrivateRoute>} />
+        <Route path="/settings"        element={<PrivateRoute><AccountSettings /></PrivateRoute>} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/reset-password"  element={<ResetPassword />} />
-        <Route path="/settings"        element={<PrivateRoute><AccountSettings /></PrivateRoute>} />
+        <Route path="*"                element={<Navigate to="/" />} />
       </Routes>
     </>
   );
